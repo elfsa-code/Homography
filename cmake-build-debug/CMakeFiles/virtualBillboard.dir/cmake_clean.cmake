2a6408265c9a254d7c0e0224f11f919a6b87e6da
elfsa-code <elfsaikou@gmail.com>
2023-06-15 09:16:02 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/virtualBillboard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/virtualBillboard.dir/virtual-billboard.cpp.o"
+  "virtualBillboard"
+  "virtualBillboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/virtualBillboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
